034e525395cd004347e58ce946ae55b883ab26e3
Angel Spasov <aspasov_@abv.bg>
2023-02-10 01:52:14 +0200

    setup project

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="navbar is-fixed-top">
+    <div class="navbar-brand">
+      <a href="https://bulma.io">
+        <img src="../../assets/images/logo.png" width="100">
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <a class="navbar-item">
+          Home
+        </a>
+
+        <a class="navbar-item">
+          Puzzles
+        </a>
+
+        <a class="navbar-item">
+          Solutions
+        </a>
+
+        <a class="navbar-item">
+          Collections
+        </a>
+
+        <a class="navbar-item">
+          Marketplace
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Useful links
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              Where to buy
+            </a>
+            <a class="navbar-item">
+              Forums
+            </a>
+            <a class="navbar-item">
+              Other sites
+            </a>
+          </div>
+        </div>
+
+        <a class="navbar-item">
+          About
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/layouts/DesktopLayout.vue
+++ src/layouts/DesktopLayout.vue
@@ -1,9 +1,42 @@
 <script setup>
-
+import Header from '../components/layout/Header.vue';
 </script>
 
 <template>
-  <div>
-    DESKTOP LAYOUT
+  <div class="tile is-vertical">
+    <Header />
+    <div>MAIN</div>
+    <div class="tile pt-4 mt-6">
+      <div class="tile is-parent is-outlined">
+        <article class="tile is-child notification is-success">
+          <div class="tile is-vertical">
+            <button class="button is-primary">
+              Button
+            </button>
+            <button class="button is-link">
+              Button
+            </button>
+            <button class="button is-info">
+              Button
+            </button>
+            <button class="button is-success">
+              Button
+            </button>
+            <button class="button is-warning">
+              Button
+            </button>
+            <button class="button is-danger">
+              Button
+            </button>
+            <button class="bulma-delete-mixin is-large"></button>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-9">
+        <div>Cubix</div>
+      </div>
+    </div>
+    <footer>Some footer text</footer>
   </div>
 </template>
+
