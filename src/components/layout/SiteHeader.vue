5d02df2bfad6f82b418ee0c6d97a4ebc8d8ef82b
Angel Spasov <aspasov_@abv.bg>
2023-03-04 23:35:44 +0200

    header: fix design

--- src/components/layout/SiteHeader.vue
+++ src/components/layout/SiteHeader.vue
@@ -1,179 +1,181 @@
 <template>
   <header>
-    <nav class="navbar is-warning" role="navigation" aria-label="main navigation" style="width: 100%;" >
+    <nav class="navbar is-warning" role="navigation" aria-label="main navigation" style="width: 100%;">
       <div class="navbar-brand">
         <a class="navbar-item" href="https://bulma.io">
           <img src="../../assets/images/logo2.png" style="max-height: 100%" width="260">
         </a>
       </div>
       <div style="width: 100%; padding: 10px 30px;">
-      <nav class="level" style="height: 40px;   margin: 0; width: 100%">
-        <div class="level-right" style="margin-left: auto">
-          <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><strong>Home</strong></p>
-          <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>About</a></p>
-          <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>Usefull links</a></p>
-          <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>Log in</a></p>
-          <p class="level-item"><a>Register</a></p>
-        </div>
-      </nav>
-      <div class="navbar-menu" style="height: 40px; align-self: flex-end;">
-        <div class="navbar-start">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Puzzles
-            </a>
-            <div class="navbar-dropdown">
-              <div class="tile is-ancestor box">
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 200px;">
-                    <p class="title is-4">Information</p>
-                    <hr>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 200px;">
-                    <p class="title is-4">Solutions</p>
-                    <hr>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 200px;">
-                    <p class="title is-4">Collections</p>
-                    <hr>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
+        <nav class="level" style="height: 40px;   margin: 0; width: 100%">
+          <div class="level-right" style="margin-left: auto">
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><strong>Home</strong></p>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>About</a></p>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>Usefull links</a></p>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px;"><a>Log in</a></p>
+            <p class="level-item"><a>Register</a></p>
+          </div>
+        </nav>
+        <div class="navbar-menu" style="height: 40px; align-self: flex-end;">
+          <div class="navbar-start">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                Puzzles
+              </a>
+              <div class="navbar-dropdown">
+                <div class="tile is-ancestor box">
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Information</p>
+                      <hr>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Solutions</p>
+                      <hr>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Collections</p>
+                      <hr>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Feed
-            </a>
-            <div class="navbar-dropdown">
-              <div class="tile is-ancestor box">
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 200px;">
-                    <p class="title is-4">Articles</p>
-                    <hr>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 200px;">
-                    <p class="title is-4">Forum</p>
-                    <hr>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subtitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item">Subtbt btitle</a>
-                    <a class="navbar-item">Subbt itle</a>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child" style="width: 600px;">
-                    <p class="title is-4">Featured</p>
-                    <hr>
-                    <article class="message">
-                      <div class="message-header">
-                        <p>Hello World</p>
-                        <button class="delete" aria-label="delete"></button>
-                      </div>
-                      <div class="message-body">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
-                        tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus. Nullam
-                        gravida purus diam, et dictum <a>felis venenatis</a> efficitur...
-                      </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                Feed
+              </a>
+              <div class="navbar-dropdown">
+                <div class="tile is-ancestor box">
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Articles</p>
+                      <hr>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item is-active">More...</a>
                     </article>
-                    <article class="message is-info">
-                      <div class="message-header">
-                        <p>Info</p>
-                        <button class="delete" aria-label="delete"></button>
-                      </div>
-                      <div class="message-body">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
-                        tempus quis placerat ut, porta nec nulla...
-                      </div>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Forum</p>
+                      <hr>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item is-active">More...</a>
                     </article>
-                    <a class="navbar-item is-active">More...</a>
-                  </article>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 600px;">
+                      <p class="title is-4">Featured</p>
+                      <hr>
+                      <article class="message">
+                        <div class="message-header">
+                          <p>Hello World</p>
+                          <button class="delete" aria-label="delete"></button>
+                        </div>
+                        <div class="message-body">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
+                          tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus. Nullam
+                          gravida purus diam, et dictum <a>felis venenatis</a> efficitur...
+                        </div>
+                      </article>
+                      <article class="message is-info">
+                        <div class="message-header">
+                          <p>Info</p>
+                          <button class="delete" aria-label="delete"></button>
+                        </div>
+                        <div class="message-body">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
+                          tempus quis placerat ut, porta nec nulla...
+                        </div>
+                      </article>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <a class="navbar-item">
-            Marketplace
-          </a>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              More
+            <a class="navbar-item">
+              Marketplace
             </a>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
 
-            <div class="navbar-dropdown">
-              <a class="navbar-item">
-                About
-              </a>
-              <a class="navbar-item">
-                Jobs
-              </a>
-              <a class="navbar-item">
-                Contact
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item">
-                Report an issue
-              </a>
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a class="button is-light">
-                Log in
-              </a>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="field has-addons">
+                <p class="control">
+                  <input class="input is-rounded" type="text" placeholder="Find a post">
+                </p>
+                <p class="control">
+                  <button class="button is-rounded">
+                    Search
+                  </button>
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </nav>
   </header>
 </template>
