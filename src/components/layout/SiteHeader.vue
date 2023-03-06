f1046b9e47fa2f6bfacdabb6b3185ce4c748154f
Angel Spasov <aspasov_@abv.bg>
2023-03-07 00:00:11 +0200

    header: fix design

--- src/components/layout/SiteHeader.vue
+++ src/components/layout/SiteHeader.vue
@@ -9,17 +9,17 @@
       <div style="width: 100%; padding: 6px 20px;">
         <nav class="level" style="height: 40px;   margin: 0; width: 100%">
           <div class="level-right" style="margin-left: auto">
-            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px; margin: 0;"><strong>Home</strong>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 20px; margin: 0;"><strong>Home</strong>
             </p>
-            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px; margin: 0;"><a>About</a></p>
-            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px; margin: 0;"><a>Usefull links</a>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 20px; margin: 0;"><a>About</a></p>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 20px; margin: 0;"><a>Usefull links</a>
             </p>
-            <p class="level-item" style="border-right: 1px solid gray; padding: 0 10px; margin: 0;"><a>Log in</a></p>
-            <p class="level-item" style="padding: 0 10px; margin: 0;"><a>Register</a></p>
+            <p class="level-item" style="border-right: 1px solid gray; padding: 0 20px; margin: 0;"><a>Log in</a></p>
+            <p class="level-item" style="padding: 0 10px; margin: 0;"><a>Join Cubix</a></p>
           </div>
         </nav>
         <div class="navbar-menu" style="height: 40px; align-self: flex-end;">
-          <div class="navbar-start">
+          <div class="navbar-end">
             <div class="navbar-item has-dropdown is-hoverable">
               <a class="navbar-link">
                 Puzzles
@@ -161,8 +161,79 @@
                 </a>
               </div>
             </div>
+            <div class="navbar-item is-hoverable" style="margin-left: 200px;">
+              <button class="button is-rounded is-link" style="width: 220px;">
+                <span class="icon is-small is-left" style="display: inline-block; margin-right: 10px;">
+                  <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                </span>
+                SEARCH
+              </button>
+              <div class="navbar-dropdown">
+                <div class="tile is-ancestor box">
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Articles</p>
+                      <hr>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 200px;">
+                      <p class="title is-4">Forum</p>
+                      <hr>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subtitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item">Subtbt btitle</a>
+                      <a class="navbar-item">Subbt itle</a>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
+                  <div class="tile is-parent">
+                    <article class="tile is-child" style="width: 600px;">
+                      <p class="title is-4">Featured</p>
+                      <hr>
+                      <article class="message">
+                        <div class="message-header">
+                          <p>Hello World</p>
+                          <button class="delete" aria-label="delete"></button>
+                        </div>
+                        <div class="message-body">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
+                          tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus. Nullam
+                          gravida purus diam, et dictum <a>felis venenatis</a> efficitur...
+                        </div>
+                      </article>
+                      <article class="message is-info">
+                        <div class="message-header">
+                          <p>Info</p>
+                          <button class="delete" aria-label="delete"></button>
+                        </div>
+                        <div class="message-body">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
+                          tempus quis placerat ut, porta nec nulla...
+                        </div>
+                      </article>
+                      <a class="navbar-item is-active">More...</a>
+                    </article>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="navbar-end">
+          <!-- <div class="navbar-end">
             <div class="navbar-item">
               <div class="field has-addons">
                 <p class="control has-icons-left">
@@ -178,7 +249,7 @@
                 </p>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </nav>
