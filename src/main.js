6f49146c9dab44ec398795e1b140104683f5adee
Angel Spasov <aspasov_@abv.bg>
2023-03-08 01:30:36 +0200

    setup project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import './assets/main.scss';
-import FontAwesomeIcon from "./assets/FontAwesomeIcons.js";
+import './assets/sass/main.scss';
+import FontAwesomeIcon from "./assets/icons/FontAwesomeIcons.js";
 
 createApp(App)
   .component("FontAwesomeIcon", FontAwesomeIcon)
