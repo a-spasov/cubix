fd86dff0007f5727569a4f2cb5d80475095bf86d
Angel Spasov <aspasov_@abv.bg>
2023-03-24 01:33:37 +0200

    setup project

--- src/assets/icons/FontAwesomeIcons.js
+++ src/assets/icons/FontAwesomeIcons.js
@@ -2,8 +2,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUser as fasUser, faLightbulb, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faUser as farUser } from "@fortawesome/free-regular-svg-icons";
+import { faYoutube, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 
-library.add(farUser, fasUser, faLightbulb, faMagnifyingGlass);
+
+library.add(farUser, fasUser, faLightbulb, faMagnifyingGlass, faYoutube, faFacebook, faTwitter);
 
 export default FontAwesomeIcon;
