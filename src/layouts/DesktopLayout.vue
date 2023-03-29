df17b33b9e5438d32a53029f3fee8838cf1e5e25
Angel Spasov <aspasov_@abv.bg>
2023-03-30 02:34:58 +0300

    setup project: typography

--- src/layouts/DesktopLayout.vue
+++ src/layouts/DesktopLayout.vue
@@ -43,7 +43,13 @@ import SiteFooter from '../components/layout/SiteFooter.vue';
     </aside>
     <section class="main section">
       <article>
-        <h1 class="title">Why do we use it?</h1>
+        <h1 class="">Why do we use it?</h1>
+        <h2 class="">Why do we use it?</h2>
+        <h3 class="">Why do we use it?</h3>
+        <h4 class="">Why do we use it?</h4>
+        <h5 class="">Why do we use it?</h5>
+        <h6 class="">Why do we use it?</h6>
+
         <p>
           It is a long established fact that a reader will be distracted by the readable content of a page when looking at
           its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
