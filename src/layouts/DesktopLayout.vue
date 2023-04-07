3a419d1b6b5a2bd1728a402c4570d9924cee07d1
Angel Spasov <aspasov_@abv.bg>
2023-04-07 08:29:35 +0300

    setup project: typography

--- src/layouts/DesktopLayout.vue
+++ src/layouts/DesktopLayout.vue
@@ -62,6 +62,7 @@ import SiteFooter from '../components/layout/SiteFooter.vue';
           will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
           accident, sometimes on purpose (injected humour and the like).
         </p>
+        <button class="button">Action</button>
       </article>
       <article style="flex: 1 1 0;">
         <h1 class="display">Mine samples:</h1>
@@ -84,6 +85,15 @@ import SiteFooter from '../components/layout/SiteFooter.vue';
           will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
           accident, sometimes on purpose (injected humour and the like).
         </p>
+        <p class="body-2">
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at
+          its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+          packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'
+          will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+        <button class="button btn">Action</button>
       </article>
     </section>
     <SiteFooter class="footer" />
